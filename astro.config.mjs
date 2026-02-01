4117b6c2f5716b8c90b0685aa534542e96bd5a07
Florian Viorel Tanely <izkx73@gmail.com>
2026-02-01 20:00:02 +0800

    Use Token template

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  site: "https://token-template.deno.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
+
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-high-contrast",
+    },
+  },
+
+  adapter: cloudflare(),
+});
