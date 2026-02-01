b3cb95f838826dcc3a478dded1f440bc642da260
Florian Viorel Tanely <izkx73@gmail.com>
2026-02-01 22:00:12 +0800

    Initial scaffolding

--- src/consts.ts
+++ src/consts.ts
@@ -1,25 +1,44 @@
-export const SITE_TITLE = "Token";
+export const SITE_TITLE = "Flo's";
 export const SITE_DESCRIPTION =
-  `Token Astro + Tailwind CSS starter featuring a sleek black-and-white terminal-inspired design, with support for RSS, sitemap, MD/MDX blog posts static content.`.trim();
+  `Florian's portfolio website to showcase my experience and skills.`.trim();
 
 export const KNOWN_TECH =
-  `Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX,Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX`.split(
+  `Java,Go,TypeScript,React,Docker`.split(
     ",",
   );
+export const NAME = "Florian Viorel Tanely";
 export const ABOUT_ME =
-  `Token is a modern and minimalistic Astro + Tailwind CSS starter designed for developers, students, and creators looking to build clean, fast, and functional websites. Featuring a sleek black-and-white theme with a custom terminal-inspired font, this template is perfect for personal portfolios, blogs, or any static content-driven website. The template comes packed with features like Markdown (MD) and MDX support for writing blog posts or documentation, allowing for flexibility and ease of use. It includes Tailwind CSS for effortless styling and customization, enabling users to tweak the design to match their branding or personal preferences without hassle. Additionally, Token integrates essential utilities like RSS feed generation, a sitemap for SEO optimization, and pre-configured components for seamless content management. Everything is static, making the site incredibly fast, lightweight, and easy to deploy on platforms like Vercel, Netlify, or Deno Deploy. Whether you're a programmer showcasing your projects, a writer sharing your ideas, or someone looking to establish an online presence, this template provides all the tools you need in a stylish and functional package. With its developer-first approach and clean codebase, Token offers a strong foundation for creating visually appealing websites that stand out while keeping performance and simplicity at the forefront.`.trim();
-export const GITHUB_USERNAME = "ArnavK-09";
-export const QUOTE = "Programmer, Gamer, Editor and Student";
+  `Hello! I'm a computer science student at National University of Singapore (NUS) passionate about crafting solutions to solve real-world challenges.`.trim();
+export const GITHUB_USERNAME = "iZUMi-kyouka";
+export const QUOTE = "Software Engineer and Problem Solver";
 export const NAV_LINKS: Array<{ title: string; href?: string }> = [
   {
     title: "Blog",
   },
+];
+export const EXPERIENCES = [
   {
-    title: "Github",
-    href: "//github.com/" + GITHUB_USERNAME,
+    title: "Software Engineering",
+    company: "Computing for Voluntary Welfare Organisations (CVWO)",
+    type: "Internship",
+    date: "May 2025 - August 2025",
+    url: "https://www.comp.nus.edu.sg/~vwo/projects/2025-cms.html",
+    description: [
+      "Maintained CVWO's Case Management System (CMS) to accommodate evolving business needs",
+      "Enhanced the system with new features and improvements to boost volunteer productivity",
+      "Migrated and integrated the CMS into the modern, flexible Cornerstone platform to streamline user experience across beneficiaries",
+      "Onboarded Haven Counselling Centre (HCC) to the system",
+    ],
   },
   {
-    title: "Source",
-    href: "//github.com/ArnavK-09/token-template",
-  },
+    title: "Teaching Assistant",
+    company: "National University of Singapore (NUS)",
+    type: "Part-time",
+    date: "August 2025 - November 2025",
+    description: [
+      "Taught a tutorial class of 6 students for the \"CS1101S Programming Methodology I\" course",
+      "Prepared weekly tutorial slides and hold consultations with students",
+      "Grade and provide feedback on their programming assignments",
+    ],
+  }
 ];
